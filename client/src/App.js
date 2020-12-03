9b1d52f7c11ea988b0cdf97b41423c54ac9f337b
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-12-03 10:34:52 -0800

    added client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Web 36 Rocks</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+const path = require('path'); 
 
 console.log("web 36 rocks");
 console.log(__dirname);
@@ -15,9 +16,15 @@ console.log(process.env.PORT);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/*', ( _, res ) => {
     res.json({ data: 'web 36 rocks '});
+})
+
+app.use('*', (_, res) => {
+    //send back index.html
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
 
 app.listen(port, () => {
